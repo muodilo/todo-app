6a130fdfc06de74a504813a27cddb0d015cf851c
muodilo <murindahabiodilo@gmail.com>
2025-04-19 00:15:06 +0200

    Set up todo list, input fields, and initial state

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const titleDateEl = document.querySelector('#title-date');
-const now = new Date().toString().slice(0,16);
+const now = new Date().toString().slice(0, 16);
 titleDateEl.innerText = now;
+
+const todoList = document.getElementById('todo-list');
+const titleInput = document.getElementById('todo-title');
+const dueInput = document.getElementById('todo-due');
+const filterCompletedCheckbox = document.getElementById('filter-completed');
+
+let todos = [];
+let isAscending = true;
